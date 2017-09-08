ce43f97889c9a9188e17024002a0b3d7886af455
weilikai01 <weilikai01@baidu.com>
2017-09-08 18:33:13 +0800

    custom parse servers

--- index.js
+++ index.js
@@ -3,6 +3,8 @@
 
 var express = require('express');
 var ParseServer = require('parse-server').ParseServer;
+var ParseDashboard = require('parse-dashboard');
+var Parse = require('parse/node');
 var path = require('path');
 
 var databaseUri = process.env.DATABASE_URI || process.env.MONGODB_URI;
@@ -11,16 +13,8 @@ if (!databaseUri) {
   console.log('DATABASE_URI not specified, falling back to localhost.');
 }
 
-var api = new ParseServer({
-  databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
-  cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-  appId: process.env.APP_ID || 'myAppId',
-  masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
-  serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
-  liveQuery: {
-    classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
-  }
-});
+
+
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
 // javascriptKey, restAPIKey, dotNetKey, clientKey
@@ -30,26 +24,77 @@ var app = express();
 // Serve static assets from the /public folder
 app.use('/public', express.static(path.join(__dirname, '/public')));
 
-// Serve the Parse API on the /parse URL prefix
-var mountPath = process.env.PARSE_MOUNT || '/parse';
-app.use(mountPath, api);
+var allowInsecureHTTP = false;
 
-// Parse Server plays nicely with the rest of your web routes
-app.get('/', function(req, res) {
-  res.status(200).send('I dream of being a website.  Please star the parse-server repo on GitHub!');
-});
 
-// There will be a test page available on the /test path of your server url
-// Remove this before launching your app
-app.get('/test', function(req, res) {
-  res.sendFile(path.join(__dirname, '/public/test.html'));
-});
 
 var port = process.env.PORT || 1337;
 var httpServer = require('http').createServer(app);
-httpServer.listen(port, function() {
+httpServer.listen(1337, function() {
     console.log('parse-server-example running on port ' + port + '.');
 });
+
+
+const runningParses = {};
+
+function reload(app, appId, masterKey) {
+  if (!appId || !masterKey) {
+    return;
+  }
+  if (runningParses[appId]) {
+    console.warn(appId + '已经在运行');
+    return;
+  }
+  runningParses[appId] = appId;
+
+  var api = new ParseServer({
+    databaseURI: databaseUri || 'mongodb://localhost:27017/' + appId,
+    cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
+    appId: appId,
+    masterKey: masterKey, //Add your master key here. Keep it secret!
+    serverURL: 'http://localhost:1337/' + appId,  // Don't forget to change to https if needed
+    liveQuery: {
+      classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
+    }
+  });
+
+  var dashboard = new ParseDashboard({
+    "apps": [
+      {
+        "serverURL": 'http://localhost:1337/' + appId,
+        "appId": appId,
+        "masterKey": masterKey,
+        "appName": appId
+      }
+    ]
+  }, allowInsecureHTTP);
+
+  app.use('/' + appId, api);
+  app.use('/dashboard-' + appId + '', dashboard);
+}
+
+
+Parse.initialize('admin', 'admin');
+Parse.serverURL = 'http://localhost:1337/admin';
+
+reload(app, 'admin', 'admin');
+
+
+setInterval(()=>{
+  new Parse.Query(new Parse.Object('ParseInfo')).find().then(items => {
+    items.map((item, index) => {
+      var appId = item.get('appId');
+      var masterKey = item.get('masterKey');
+      console.log(appId, masterKey, index);
+
+      reload(app, appId, masterKey);
+    });
+
+
+  }, e=> {
+    console.log(e);
+  });
+}, 1000);
 
 // This will enable the Live Query real-time server
 ParseServer.createLiveQueryServer(httpServer);
